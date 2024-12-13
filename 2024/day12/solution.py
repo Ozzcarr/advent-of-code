0ee4a6d6786f2b036838055c654f48b00c33d46a
Oscar Andersson <anderssonoscar03@gmail.com>
2024-12-13 04:20:35 +0100

    2024 Day 12 Part 2

--- 2024/day12/solution.py
+++ 2024/day12/solution.py
@@ -1,40 +1,73 @@
 """Advent of Code 2024 - Day 12"""
 
 import os
+from collections import defaultdict
 
 
-def floodfill(grid: list[str], r: int, c: int, color: str, checked_coordinates: set) -> tuple[int, int, int]:
+def floodfill(grid: list[str], r: int, c: int, color: str, checked_coordinates: set) -> tuple[int, int]:
     if not 0 <= r < len(grid) or not 0 <= c < len(grid[0]):
-        return 0, 1, checked_coordinates
+        return 0, 1
     if grid[r][c] != color or (r, c) in checked_coordinates:
-        return 0, 1 if grid[r][c] != color else 0, checked_coordinates
+        return 0, 1 if grid[r][c] != color else 0
 
     checked_coordinates.add((r, c))
     total_area = 1
     total_perimeter = 0
 
     for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        area, perimeter, region = floodfill(grid, r + dr, c + dc, color, checked_coordinates)
+        area, perimeter = floodfill(grid, r + dr, c + dc, color, checked_coordinates)
         total_area += area
         total_perimeter += perimeter
 
-    return total_area, total_perimeter, checked_coordinates
+    return total_area, total_perimeter
 
 
-def sides(region: set, width: int, height: int) -> int:
-    return 0
+def floodfill2(grid: list[str], r: int, c: int, color: str, visited: list[str], sides) -> int:
+    if not 0 <= r < len(grid) or not 0 <= c < len(grid[0]) or visited[r][c] is not None or grid[r][c] != color:
+        return 0
+    visited[r][c] = color
+    total_area = 1
+
+    for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        nr, nc = r + dr, c + dc
+        if not 0 <= nr < len(grid) or not 0 <= nc < len(grid[0]) or grid[nr][nc] != color:
+            if c != nc:
+                sides[('v', c, c > nc)].append(r)
+            else:
+                sides[('h', r, r > nr)].append(c)
+        else:
+            total_area += floodfill2(grid, nr, nc, color, visited, sides)
+
+    return total_area
+
+
+def calculate_sides(sides) -> int:
+    total_sides = 0
+    for _, side_row in sides.items():
+        side_row.sort()
+        for i in range(1, len(side_row)):
+            if (side_row[i - 1] + 1) != side_row[i]:
+                total_sides += 1
+        total_sides += 1
+    return total_sides
 
 
 def price(grid: list[str], part2=False) -> int:
     checked_coordinates = set()
     total_price = 0
+    visited = [[None] * len(grid[0]) for _ in range(len(grid))]
 
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             if (r, c) not in checked_coordinates:
-                area, perimeter, region = floodfill(grid, r, c, grid[r][c], set())
-                checked_coordinates = checked_coordinates.union(region)
-                total_price += area * (sides(region, len(grid), len(grid[0])) if part2 else perimeter)
+                if part2:
+                    sides = defaultdict(list)
+                    area = floodfill2(grid, r, c, grid[r][c], visited, sides)
+                    sides = calculate_sides(sides)
+                    total_price += area * sides
+                else:
+                    area, perimeter = floodfill(grid, r, c, grid[r][c], checked_coordinates)
+                    total_price += area * perimeter
 
     return total_price
 
